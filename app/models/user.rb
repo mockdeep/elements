612616506232f7b7753e4868381a4c323d54ff2c
Robert Fletcher <lobatifricha@gmail.com>
2011-11-28 13:10:20 -0800

    adding user login pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,30 @@
 require 'uuid_helper'
+require 'constants'
 
 class User < ActiveRecord::Base
   include UUIDHelper
+  attr_accessor :password
+  attr_accessible :email, :username, :password, :password_confirmation
+  before_save :encrypt_password
+
   validates_presence_of :email
   validates_presence_of :username
+  validates_presence_of :password, :on => :create
+  validates_confirmation_of :password
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => Constants::EMAIL_REGEX
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return nil unless user
+    hash = BCrypt::Engine.hash_secret(password, user.password_salt)
+    user.password_hash == hash ? user : nil
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,10 @@ module Todo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
