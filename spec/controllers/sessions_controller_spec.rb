82f41706fef650f276f49789ccd9e177a7958b8e
Robert Fletcher <lobatifricha@gmail.com>
2012-01-28 16:35:47 -0800

    adding sessions_controller specs

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe SessionsController do
+  before :each do
+    @user = Factory(:user)
+  end
+
+  describe '#create' do
+    context 'given valid credentials' do
+      before :each do
+        post :create, :email => @user.email, :password => @user.password
+      end
+
+      it 'sets the session user_id' do
+        session[:user_id].should == @user.id
+      end
+
+      it 'flashes a success message' do
+        flash[:notice].should == 'Logged in!'
+      end
+
+      it 'redirects to elements#index' do
+        response.should redirect_to(elements_index_path)
+      end
+    end
+
+    context 'given invalid credentials' do
+      before :each do
+        post :create, :email => @user.email, :password => 'wrongpassword'
+      end
+
+      it 'sets the session user_id to nil' do
+        session[:user_id].should be_nil
+      end
+
+      it 'flashes an error message' do
+        flash.now[:error].should == 'Invalid email or password'
+      end
+
+      it 'renders the new template' do
+        response.should render_template('sessions/new')
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before :each do
+      session[:user_id] = @user.id
+      delete :destroy
+    end
+
+    it 'sets the session user_id to nil' do
+      session[:user_id].should be_nil
+    end
+
+    it 'flashes a logged out message' do
+      flash[:notice].should == 'Logged out!'
+    end
+
+    it 'redirects to the root url' do
+      response.should redirect_to(root_url)
+    end
+  end
+end
