8f833aa50e04be01566cece5d6b7991b9834fc7a
Robert Fletcher <lobatifricha@gmail.com>
2012-01-30 18:01:21 -0800

    refactoring user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,49 +1,67 @@
 require 'spec_helper'
 
 describe User do
-  it { should have_many :elements }
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :username }
-  it { should validate_presence_of :password }
+  describe '#elements' do
+    it { should have_many :elements }
+  end
 
-  describe 'password encryption' do
+  describe '#valid?' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :password }
+  end
+
+  describe '#encrypt_password' do
     before :each do
       @user = User.new
     end
 
-    it 'should do nothing if password is blank' do
-      @user.encrypt_password
-      @user.password_salt.should be_nil
-      @user.password_hash.should be_nil
-      @user.password.should be_nil
+    context 'when password is blank' do
+      it 'does nothing' do
+        @user.encrypt_password
+        @user.password_hash.should be_nil
+      end
     end
 
-    it 'should encrypt if password is given' do
-      @user.password = 'blah'
-      @user.password_confirmation = 'blah'
-      @user.encrypt_password
+    context 'when password is given' do
+      before :each do
+        @user.password = 'blah'
+        @user.password_confirmation = 'blah'
+        @user.encrypt_password
+      end
 
-      @user.password_salt.should_not be_nil
-      hash = BCrypt::Engine.hash_secret('blah', @user.password_salt)
-      @user.password_hash.should == hash
+      it 'generates a password salt' do
+        @user.password_salt.should_not be_nil
+      end
+
+      it 'encrypts password' do
+        hash = BCrypt::Engine.hash_secret('blah', @user.password_salt)
+        @user.password_hash.should == hash
+      end
     end
   end
 
-  describe 'authentication' do
+  describe '#authenticate' do
     before :each do
       @user = Factory(:user)
     end
 
-    it 'should return the user given valid credentials' do
-      User.authenticate(@user.email, @user.password).should == @user
+    context 'given valid credentials' do
+      it 'returns the user object' do
+        User.authenticate(@user.email, @user.password).should == @user
+      end
     end
 
-    it 'should return nil given bad password' do
-      User.authenticate(@user.email, 'nonsense').should be_nil
+    context 'given a bad password' do
+      it 'returns nil' do
+        User.authenticate(@user.email, 'nonsense').should be_nil
+      end
     end
 
-    it 'should return nil given non-existent email' do
-      User.authenticate('nonsense', @user.password).should be_nil
+    context 'given a non-existent email' do
+      it 'returns nil' do
+        User.authenticate('nonsense', @user.password).should be_nil
+      end
     end
   end
 end
