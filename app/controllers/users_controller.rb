984e783f5373dc929f735cbce0594acc1ce7f825
Robert Fletcher <lobatifricha@gmail.com>
2012-12-13 00:25:22 -0800

    wrapping long lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
--- spec/javascripts/support/jasmine_runner.rb
+++ spec/javascripts/support/jasmine_runner.rb
@@ -1,8 +1,10 @@
-$:.unshift(ENV['JASMINE_GEM_PATH']) if ENV['JASMINE_GEM_PATH'] # for gem testing purposes
+# for gem testing purposes
+$:.unshift(ENV['JASMINE_GEM_PATH']) if ENV['JASMINE_GEM_PATH']
 
 require 'rubygems'
 require 'jasmine'
-jasmine_config_overrides = File.expand_path(File.join(File.dirname(__FILE__), 'jasmine_config.rb'))
+jasmine_file = File.join(File.dirname(__FILE__), 'jasmine_config.rb')
+jasmine_config_overrides = File.expand_path(jasmine_file)
 require jasmine_config_overrides if File.exist?(jasmine_config_overrides)
 if Jasmine::Dependencies.rspec2?
   require 'rspec'
